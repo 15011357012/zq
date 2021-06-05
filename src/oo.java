198806959cfeac6f81724b59920bb33cce8bffd5
15011357012 <911755862@qq.com>
2021-06-05 10:16:38 +0800

    qq

--- src/oo.java
+++ src/oo.java
@@ -1,9 +1,7 @@
 public class oo {
     int a = 1666;
     public void main(){
-        System.out.printf("::");
-        System.out.printf("::");
-        System.out.printf("::");
+
         System.out.printf("::");
     }
 }
