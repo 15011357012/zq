3103bfbcb8497e4c0da540a406f9d1dccd3231c4
15011357012 <911755862@qq.com>
2021-06-05 10:49:43 +0800

    住

--- src/oo.java
+++ src/oo.java
@@ -4,6 +4,16 @@ public class oo {
 
         System.out.printf("分" +
                 "");
+ System.out.printf("分" +
+                "");
+ System.out.printf("分" +
+                "");
+ System.out.printf("分" +
+                "");
+ System.out.printf("分" +
+                "");
+ System.out.printf("分" +
+                "");
 
 
     }
