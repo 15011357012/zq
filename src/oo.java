184e8d9b0401d1c99b4f21ce496fafa65121f2a1
15011357012 <911755862@qq.com>
2021-06-05 10:58:23 +0800

    住

--- src/oo.java
+++ src/oo.java
@@ -2,18 +2,8 @@ public class oo {
     int a = 1666;
     public void main(){
 
-        System.out.printf("分住东方闪电" +
-                "");
- System.out.printf("分" +
-                "");
- System.out.printf("分" +
-                "");
- System.out.printf("分" +
-                "");
- System.out.printf("分" +
-                "");
- System.out.printf("分" +
-                "");
+        System.out.printf("分住东方闪电" );
+
 
 
 
